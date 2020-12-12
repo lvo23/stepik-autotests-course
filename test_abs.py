a8b163a6007b71ed37da1d582e1f4755ecef7004
Vlad <litvinov.lvo@gmail.com>
2020-12-13 01:12:20 +0500

    поправил длину строк, продолжил тест

--- test_abs.py
+++ test_abs.py
@@ -2,5 +2,6 @@ def test_abs1():
     assert abs(-42) == 42, "Should be absolute value of a number"
 
 
+# этот тест должен упасть
 def test_abs2():
     assert abs(-42) == -42, "Should be absolute value of a number"
