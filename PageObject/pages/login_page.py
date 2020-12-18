1593b05be830f22538d09e44df673ca950d73815
Vlad <litvinov.lvo@gmail.com>
2020-12-18 15:10:23 +0500

    реализованы проверки страницы логина

--- PageObject/pages/login_page.py
+++ PageObject/pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert self.browser.current_url('login'), 'Login link is not current'
+        assert '/login' in self.browser.current_url, 'login not in current url'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
--- PageObject/test_main_page.py
+++ PageObject/test_main_page.py
@@ -26,12 +26,24 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_login_page_locators(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
+
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
     page.should_be_login_form()
+
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
     page.should_be_register_form()
 
 
