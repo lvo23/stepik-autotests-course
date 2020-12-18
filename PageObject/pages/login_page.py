8d5ffe9ae603b2832e2b0f0aab9b28f3b63e2191
Vlad <litvinov.lvo@gmail.com>
2020-12-18 14:31:19 +0500

    добавлена страница логина, новые локаторы для страницы логина, проверки на странице логина

---
+++ PageObject/pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url('login'), 'Login link is not current'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), 'Login form missing'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), 'Register form missing'
--- PageObject/test_main_page.py
+++ PageObject/test_main_page.py
@@ -1,13 +1,12 @@
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 """Основная идея состоит в том, что каждую страницу веб-приложения можно описать в виде объекта класса. Способы
 взаимодействия пользователя со страницей можно описать с помощью методов класса. В идеале тест, который будет
 использовать Page Object, должен описывать бизнес-логику тестового сценария и скрывать Selenium-методы взаимодействия
 с браузером и страницей. При изменениях в верстке страницы не придется исправлять тесты, связанные с этой страницей.
-Вместо этого нужно будет поправить только класс, описывающий страницу. """
-
-"""Важно! Обычно методы у Page Object бывают двух типов: сделать что-то и проверить что-то.
-"""
-
-from .pages.main_page import MainPage
+Вместо этого нужно будет поправить только класс, описывающий страницу.
+ Важно! Обычно методы у Page Object бывают двух типов: сделать что-то и проверить что-то."""
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -18,6 +17,22 @@ def test_guest_can_go_to_login_page(browser):
 
     browser.get(link)
     go_to_login_page(browser)
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_login_page_locators(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 
 def go_to_login_page(browser):
